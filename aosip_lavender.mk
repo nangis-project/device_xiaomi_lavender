3c940d7a53f5eb7cf2a09a8bfa8870c7f126cc39
narjirolu125 <narjirolu@gmail.com>
2020-05-11 10:48:27 +0000

    lavender: update for may fingerprint

--- aosip_lavender.mk
+++ aosip_lavender.mk
@@ -49,7 +49,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
      PRODUCT_NAME="lavender" \
      PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V10.3.9.0.PFGMIXM release-keys"
 
-BUILD_FINGERPRINT := "google/crosshatch/crosshatch:10/QQ2A.200405.005/6254899:user/release-keys"
+BUILD_FINGERPRINT := "google/crosshatch/crosshatch:10/QQ2A.200501.001.A3/6353761:user/release-keys"
 
 TARGET_VENDOR_PRODUCT_NAME := lavender
 
